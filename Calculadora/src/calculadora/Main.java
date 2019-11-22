ffd778bc08b44082e468592cd0aec4e380673b63
Lodek <23224552+Lodek@users.noreply.github.com>
2019-11-22 13:36:03 -0300

    calculadora classe

--- Calculadora/src/calculadora/Main.java
+++ Calculadora/src/calculadora/Main.java
@@ -12,3 +12,15 @@ public class Main {
         return result;
     }
 }
+
+public class Calculator{
+
+    public Calculator();
+    public double add(double x, double y){
+        return x + y;
+    }
+
+    public double minus(double x, double y){
+        return x - y;
+    }
+}
